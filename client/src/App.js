ff57019dd5760485c044c9139868a0e6471a2ecf
CCCWS <86645532+CCCWS@users.noreply.github.com>
2022-06-12 20:15:02 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainPage from "./components/views/MainPage/MainPage";
-import LoginPage from "./components/views/LoginPage/LoginPage";
-import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 import Auth from "../src/hoc/auth";
+import Main from "./Page/Main";
+import Header from "./Components/Header/Header";
 
 function App() {
-  const AuthMainPage = Auth(MainPage, null);
-  const AuthLoginPage = Auth(LoginPage, false);
-  const AuthRegisterPage = Auth(RegisterPage, false); //이거 사용하면 auth,js에서 navgate사용시 router밖이라고 에러발생
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={ Auth(MainPage, null)} />
-        <Route
-          path={`${process.env.PUBLIC_URL}/LoginPage`}
-          element={Auth(LoginPage, false)}
-        />
-        <Route
-          path={`${process.env.PUBLIC_URL}/RegisterPage`}
-          element={ Auth(RegisterPage, false)}
-        />
+        <Route path={`/`} element={Auth(Main, null)} />
       </Routes>
     </BrowserRouter>
   );
